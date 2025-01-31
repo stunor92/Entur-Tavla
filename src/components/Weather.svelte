dfd63037b6c9dde128d030aba3ad93a1e5ec5958
Sturle Tolo Nordeide <110419122+stunor92@users.noreply.github.com>
2025-01-31 14:30:30 +0100

    Use entur branding

--- src/components/Weather.svelte
+++ src/components/Weather.svelte
@@ -8,34 +8,34 @@
 
 
 
-<div class="w-full gap-4 justify-between items-center text-white">
-  <div class="">
+<div class="w-full gap-4 justify-between items-center text-grey mr-10">
+  <div class="w-full text-center text-xl">
     {weather.time.substring(11, 16)}
   </div>
   <div class="flex">
     <img
       src="/weathericons/{convertSymbolKeyToId(weather.data.next_1_hours.summary.symbol_code)}.svg"
       alt=""
-      class="w-100"/>
-    <div class=" text-xl">
+      class="w-100 mr-5"/>
+    <div class=" text-l">
       <div class="flex">
-        <img src="/thermometer.svg" class="w-7"/>
+        <img src="/thermometer.svg" class="w-7 mr-3"/>
         {#if weather.data.instant.details.air_temperature > 0}
-          <div class="text-red-500">
+          <div class="text-red-500 text-l">
             {formatNumber(weather.data.instant.details.air_temperature, 'celsius')}
           </div>
         {:else}
-          <div class="text-blue-500">
+          <div class="text-blue-500 text-l">
             {formatNumber(weather.data.instant.details.air_temperature, 'celsius')}
           </div>
         {/if}
       </div>
-      <div class="flex text-white-500">
-        <img src="/umbrella.svg" class="w-7"/>
+      <div class="flex text-500 text-l">
+        <img src="/umbrella.svg" class="w-7 mr-3"/>
         {formatNumber(weather.data.next_1_hours.details.precipitation_amount, 'millimeter')}
       </div>
-      <div class="flex text-white-500">
-        <img src="/wind.svg" class="w-7"/>
+      <div class="flex text-500 text-l">
+        <img src="/wind.svg" class="w-7 mr-3"/>
         {formatNumber(weather.data.instant.details.wind_speed, 'meter-per-second')}
       </div>
     </div>
